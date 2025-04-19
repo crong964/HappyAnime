9621c1d18d43e436a14dae9cff3675652cca6422
crong964 <huy91027@gmail.com>
2025-04-19 22:39:18 +0700

    #17 change env

--- config/GetEnv.ts
+++ config/GetEnv.ts
@@ -1,1 +1,3 @@
-export const domain=process.env.DOMAIN
+export const domain = process.env.DOMAIN
+export const revalidate = parseInt(process.env.REVALIDATE || "8 * 3600")
+export const pathimage = process.env.PATHIMGBLOG || "/"
